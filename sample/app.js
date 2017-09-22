e27e03e7bdc367085ef22ca853901c66cfce6a61
igentle11 <igentle11@gmail.com>
2017-09-23 01:38:39 +0800

    賀

--- sample/app.js
+++ sample/app.js
@@ -17,7 +17,6 @@ var bcrypt = require('bcryptjs');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-
 //create db connection using mongoose
 var db = mongoose.connection;
 
@@ -25,27 +24,11 @@ var db = mongoose.connection;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.get('/', function(request, response) {
-  response.render('index');
-});
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
